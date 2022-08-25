721ec9230a0a99629ccf903aa64ac8a24baa28a7
김도선(John) <79188587+ehtjsv2@users.noreply.github.com>
2022-08-25 17:06:25 +0900

    [Silver III] Title: 수열, Time: 268 ms, Memory: 22616 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -20,14 +20,35 @@ public class Main {
             temp[i]=Integer.parseInt(st.nextToken());
         }
 
+        int max=Integer.MIN_VALUE;
+
         /* 최대치 찾기, i+K-1가 N이상이면 종료 */
-        int max=-2147483648;
-        for(int i=0;i+K-1<N;i++){
-            int sum=0;
-            for(int j=i;j<i+K;j++){
-                sum+=temp[j];
+        
+        // for(int i=0;i+K-1<N;i++){
+        //     int sum=0;
+        //     for(int j=i;j<i+K;j++){
+        //         sum+=temp[j];
+        //     }
+        //     if(sum>max)max=sum;
+        // }
+
+        /* 연산횟수 줄이고 max찾기 */
+        int start=0,end=0;// 진행중인 첫과 끝 인덱스
+        int cnt_k=0;    // K의 어느만큼 진행됐는지 카운트
+        int sum=0;
+        while(true){
+            /* K만큼 더했다면 max인지 확인하고, 현재합의 첫인덱스 빼기*/
+            if(cnt_k>=K){ 
+                if(sum>max)max=sum;
+                sum-=temp[start++];
             }
-            if(sum>max)max=sum;
+            
+            /* 마지막인덱스까지 한번 더했다면 끝임 */
+            if(end==N)break;
+
+            /* 합구하기 */
+            sum+=temp[end++];
+            cnt_k++;
         }
         System.out.println(max);
     }
