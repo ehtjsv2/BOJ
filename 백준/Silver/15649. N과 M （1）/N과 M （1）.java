8eac498cb33f91cf4f935bfab5d640c8a5c6843e
김도선(John) <79188587+ehtjsv2@users.noreply.github.com>
2022-09-05 18:36:33 +0900

    [Silver III] Title: N과 M (1), Time: 2676 ms, Memory: 67916 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] visited;
+    static int[] array;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited=new int[N+1];
+        array=new int[N];
+        recur(0);
+    }
+    /* 백트래킹 */
+    static void recur(int index){
+        /* M개를 다골랐을경우 */
+        if(index>M-1){
+            /* 출력 */
+            for(int i=0;i<M;i++){
+                System.out.print(array[i]+" ");
+            }
+            System.out.println();
+            visited[array[index-1]]=0; // M-1로 돌아가기전에 현재 방문해있는곳 방문x로 초기화
+            return;
+        }
+        /* 1~N까지 하나씩 array에 집어넣기. 단, 방문했으면 하지않는다. */
+        for(int i=1;i<=N;i++){
+          //  System.out.println("반복시작 : "+i);
+            if(i==2){
+            //    System.out.println("현재 2 = "+visited[i]);
+            }
+            if(visited[i]==0){
+                visited[i]=1;
+                array[index]=i;
+                recur(index+1);
+            }
+        }
+        if(index>0)visited[array[index-1]]=0;
+        return ;
+    }
+}
