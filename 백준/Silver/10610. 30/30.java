7ac591e968c41771c6aac47f234f58096d80149b
김도선(John) <79188587+ehtjsv2@users.noreply.github.com>
2023-04-12 10:39:12 +0900

    [Silver IV] Title: 30, Time: 300 ms, Memory: 25480 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -2,36 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));				
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String N = st.nextToken();
-        Integer []num = new Integer[N.length()];
+public class Main{
+    static String N; // 최대 10^5개 숫자이므로 String 처리
+    static int answer=0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N=br.readLine();
 
+        Integer[] inputArray = new Integer[N.length()];
         int sum=0;
-        int IsZero=0;
-        int len=N.length();
-
+        int len = N.length();
+        //0있는지 체크, integer배열에 숫자 담기
         for(int i=0;i<len;i++){
-            num[i]=N.charAt(i)-'0';
-        }
-        for(int i=0;i<len;i++){
-            sum+=num[i];
-            if(num[i]==0)IsZero=1;
+            int ch;
+            ch=N.charAt(i)-'0';
+            sum+=ch;
+            inputArray[i]=ch;
         }
 
-        
-        if(IsZero==1 && sum%3==0){
-            Arrays.sort(num, Collections.reverseOrder());
-            for(int i=0;i<N.length();i++)
-                System.out.print(num[i]);
+        //배열 내림차순으로 정렬
+        Arrays.sort(inputArray, Collections.reverseOrder());
+        //sum이 3의 배수이고 배열에 0이 포함되어있을 때
+        if(sum%3==0 && inputArray[inputArray.length-1]==0){
+            //배열을 출력
+            StringBuffer sb= new StringBuffer();
+            for(int i=0;i<len;i++){
+                sb.append(inputArray[i]);
+            }
+            System.out.println(sb);
         }
-        else {
+        else{ // 그 외
             System.out.println("-1");
         }
     }
