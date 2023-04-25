d9236335278ad85c59f5d60f0348c18ba9867559
김도선(John) <79188587+ehtjsv2@users.noreply.github.com>
2023-04-25 17:34:51 +0900

    [Silver I] Title: 회의실 배정, Time: 508 ms, Memory: 44096 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,41 +1,51 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        int answer=0;
+    public static void main(String args[]) throws NumberFormatException, IOException{
+        int N;
+        int times[][];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] TIME = new int[N][2];
+        N=Integer.parseInt(br.readLine());
+        times = new int[N][2];
+        StringTokenizer st;
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            TIME[i][0] = Integer.parseInt(st.nextToken());
-            TIME[i][1] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine());
+            times[i][0]=Integer.parseInt(st.nextToken());
+            times[i][1]=Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(TIME, new Comparator<int[]>() {
+        /* 끝나는 시간 기준으로 정렬 */ 
+        Arrays.sort(times,new Comparator<int []>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                 if(o1[1] == o2[1]) {
-                         return o1[0] - o2[0];
-                 }else {
-                     return o1[1] - o2[1]; 
-                 }
-               }
-            });
-            
-        int accept_end_time=TIME[0][1];
-        answer=1;
-        for(int i=1;i<N;i++){
-            if(TIME[i][0]==TIME[i][1]){
-                accept_end_time=TIME[i][1];
-                answer++;
+                if(o1[1]==o2[1]){
+                    return o1[0]-o2[0];
+                }
+                else return o1[1]-o2[1];
             }
-            else if(TIME[i][0]>=accept_end_time){
-                accept_end_time=TIME[i][1];
+            
+        });
+
+        // for(int i=0;i<N;i++){
+        //     System.out.println(times[i][0]+" ~ "+times[i][1]);
+        // }
+        /* 첫시간을 배정후, 순차적으로search-> start_time이 end_time보다 크거나같으면 회의에 배정 후 end_tiem update */
+        int answer=0;
+        int end_time=0;
+        end_time=times[0][1];
+        answer++;
+        for(int i=1;i<N;i++){
+            if(times[i][0]>=end_time){
                 answer++;
+                end_time=times[i][1];
             }
         }
         System.out.println(answer);
+
     }
 }
