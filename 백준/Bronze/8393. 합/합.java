95682e4166d43523ff57f1e0a7ece95474d1bfc3
김도선(John) <79188587+ehtjsv2@users.noreply.github.com>
2023-08-20 11:50:17 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -9,10 +9,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int sum=0;
-        for(int i=1;i<=N;i++){
-            sum+=i;
-        }
-        System.out.println(sum);
+        
+        System.out.println(N*(N+1)/2);
         
 
     }
