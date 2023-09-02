9b3ebdfdfaf74f7fa25e064dda2d97b26d2de535
김도선(John) <79188587+ehtjsv2@users.noreply.github.com>
2023-09-02 16:32:41 +0900

    [Bronze II] Title: 벌집, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 1;
+        int room = 1;
+        int six = 6;
+        while(room<N){
+            answer++;
+            room+=six;
+            six+=6;
+        }
+        System.out.println(answer);
+    }
+}
