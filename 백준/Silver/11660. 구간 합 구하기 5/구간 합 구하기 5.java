b0b61db6519ec929fccbbbd77170aebd4c0b3fb0
김도선 <ehtjsv2@naver.com>
2024-07-23 09:51:47 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 928 ms, Memory: 136288 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,23 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int[][] arr;
+    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        long[][] arr = new long[N + 1][N + 1];
-        long[][] rowSum = new long[N + 1][N + 1];
-
+        arr = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                long num = Long.parseLong(st.nextToken());
-                arr[i][j] = num;
-                rowSum[i][j] = rowSum[i][j-1]+num;
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
+
+        dp = new int[N + 1][N + 1];
+        dp[1][1] = arr[1][1];
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -28,16 +31,21 @@ public class Main {
             int startJ = Integer.parseInt(st.nextToken());
             int endI = Integer.parseInt(st.nextToken());
             int endJ = Integer.parseInt(st.nextToken());
-
-            long sum = 0;
-            for (int j = startI; j <= endI; j++) {
-                int nowI = j;
-                long endSum = rowSum[nowI][endJ];
-                long minus = rowSum[nowI][startJ-1];
-                sum+=endSum-minus;
-            }
-            sb.append(sum+"\n");
+            int answer =
+                    findDp(endI, endJ) - findDp(endI, startJ-1) - findDp(startI-1, endJ) + findDp(startI - 1, startJ - 1);
+            sb.append(answer + "\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
+    }
+
+    public static int findDp(int i, int j) {
+        if (i == 0 || j == 0) {
+            return 0;
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        dp[i][j] = findDp(i - 1, j) + findDp(i, j - 1) - findDp(i - 1, j - 1) + arr[i][j];
+        return dp[i][j];
     }
 }
