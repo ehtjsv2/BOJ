24669598f75b778c71aeb6c0e9a8e7637edf0d17
김도선 <ehtjsv2@naver.com>
2024-09-29 22:39:27 +0900

    Update 정수 삼각형.java

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,31 +2,25 @@ import java.util.*;
 
 class Solution {
     
+    static int[][] dp = new int[500][500];
+    static int height;
+    static int[][] triangle;
+    
     public int solution(int[][] triangle) {
-       int answer = 0;
-        int[][] dp = new int[triangle.length+1][triangle.length];
-        
-        for(int i = 1; i<triangle.length+1; i++){
-            for(int j = 0; j<triangle[i-1].length; j++){
-                for(int k = j-1; k<j+1; k++){
-                                        
-                    if(k < 0) continue;
-                    
-                    dp[i][j] = Math.max(dp[i][j], triangle[i-1][j] + dp[i-1][k]);
-                    
-                    answer = Math.max(dp[i][j], answer);
-                }
-            }
+        this.triangle = triangle;
+        height = triangle.length;
+        for(int i=0; i<triangle[height-1].length; i++){
+            dp[height-1][i] = triangle[height-1][i];
         }
-        
+        int answer = findDp(0,0);
         return answer;
     }
     
-    // public static int findDp(int i, int j){
-    //     if(dp[i][j] != 0){
-    //         return dp[i][j];
-    //     }
-    //     dp[i][j] = triangle[i][j]+Math.max(findDp(i+1,j),findDp(i+1,j+1));
-    //     return dp[i][j];
-    // }
+    public static int findDp(int i, int j){
+        if(dp[i][j] != 0){
+            return dp[i][j];
+        }
+        dp[i][j] = triangle[i][j]+Math.max(findDp(i+1,j),findDp(i+1,j+1));
+        return dp[i][j];
+    }
 }
