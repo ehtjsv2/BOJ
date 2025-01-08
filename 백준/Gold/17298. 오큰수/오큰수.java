fd30cf0ac5a8eda437fa9df5f4088f3c2156c35e
김도선 <ehtjsv2@naver.com>
2025-01-09 08:50:33 +0900

    [Gold IV] Title: 오큰수, Time: 820 ms, Memory: 183584 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,51 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static Deque<Integer> dq;
-    static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        dq = new ArrayDeque<>();
-        answer = new int[N];
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] answer = new int[N];
+        Arrays.fill(answer,-1);
+        Deque<Integer> order = new ArrayDeque<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            int j = i;
             while (!dq.isEmpty() && dq.peekFirst() < num) {
-                int answerIndex = findAnswerIndex(j);
-                answer[answerIndex] = num;
+                answer[order.removeFirst()] = num;
                 dq.removeFirst();
-                j--;
             }
             dq.addFirst(num);
+            order.addFirst(i);
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            if (answer[i] == 0) {
-                sb.append(-1).append(" ");
-            }
-            else{
-                sb.append(answer[i]).append(" ");
-            }
+            sb.append(answer[i]).append(" ");
         }
         System.out.println(sb);
-    }
-
-    private static int findAnswerIndex(int i) {
-        while (answer[i - 1] != 0) {
-            i--;
-        }
-        return i - 1;
     }
 }
