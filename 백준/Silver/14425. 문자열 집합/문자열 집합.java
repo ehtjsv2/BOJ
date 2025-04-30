bdbde017d2fc1e9ce16c68116d783e2e2f698fa8
김도선 <ehtjsv2@naver.com>
2025-04-30 11:50:04 +0900

    [Silver IV] Title: 문자열 집합, Time: 320 ms, Memory: 31144 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -5,22 +5,17 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<String,Boolean> HM = new HashMap<String,Boolean>();
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         String S[] =new String[N];
         String test[] = new String[M];
-        for(int i=0;i<N;i++)S[i]=br.readLine();
+        for(int i=0;i<N;i++)HM.put(br.readLine(),true);
         for(int i=0;i<M;i++)test[i]=br.readLine();
 
         int answer=0;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(S[i].equals(test[j])){
-                    // System.out.println("->"+test[j]);
-                    answer++;
-                }
-            }
-            
+        for(int i=0;i<M;i++){
+            if(HM.containsKey(test[i]))answer++;
         }
         System.out.println(answer);
     }
