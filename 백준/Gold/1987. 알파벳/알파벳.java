ce44211d4ddc25ebe62d2088800ce5dde4851fdf
김도선 <ehtjsv2@naver.com>
2025-05-08 17:00:42 +0900

    [Gold IV] Title: 알파벳, Time: 2008 ms, Memory: 303432 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] di = {0, 1, 0, -1};
+    static int[] dj = {1, 0, -1, 0};
+    static int N;
+    static int M;
+    static char[][] arr;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        HashSet<Character> set = new HashSet<>();
+        set.add(arr[0][0]);
+        dfs(0,0,set,1);
+        System.out.println(answer);
+    }
+
+    public static void dfs(int i, int j, HashSet<Character> set, int depth) {
+        if (answer < depth) {
+            answer = depth;
+        }
+        for (int k = 0; k < 4; k++) {
+            int nextI = i+di[k];
+            int nextJ = j+dj[k];
+            if (nextI >= N || nextJ >= M || nextJ < 0 || nextI < 0 || set.contains(arr[nextI][nextJ])) {
+                continue;
+            }
+            set.add(arr[nextI][nextJ]);
+            dfs(nextI, nextJ, set, depth + 1);
+            set.remove(arr[nextI][nextJ]);
+        }
+    }
+}
