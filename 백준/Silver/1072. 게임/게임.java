a2d6dd8a448dab0886bde75b2eed1f985e860787
김도선 <ehtjsv2@naver.com>
2025-05-13 17:34:13 +0900

    [Silver III] Title: 게임, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -8,29 +8,44 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int X = Integer.parseInt(st.nextToken());
-        int Y = Integer.parseInt(st.nextToken());
-        int Z = getPercent(X, Y);
+        long x = Long.parseLong(st.nextToken());
+        long y = Long.parseLong(st.nextToken());
+        long z = 100 * y / x;
+        if (z == 100 || z == 99) {
+            System.out.println(-1);
+            return;
+        }
 
-        int ans = -1;
-        int low = 0;
-        int high = X + 1;
-        while (low + 1 < high) {
-            int mid = (low + high) / 2;
-            int Z2 = getPercent((X + mid), (Y + mid));
-            if (Z2 > Z) {
-                ans = mid;
-                high = mid;
+        long y2 = y + 1;
+        long x2 = x + 1;
+        long count = 2;
+        long beforeY2 = y;
+        long beforeX2 = x;
+        long z2 = 100 * y2 / x2;
+        while (z == z2) {
+            beforeY2 = y2;
+            beforeX2 = x2;
+            y2 += count;
+            x2 += count;
+            count*=2;
+            z2 = 100 * y2 / x2;
+        }
+
+        long left = beforeY2 - y;
+        long right = y2 - y;
+        long middle = (right + left) / 2;
+        while (left < right) {
+            middle = (right + left) / 2;
+            x2 = x + middle;
+            y2 = y + middle;
+            z2 = 100 * y2 / x2;
+            if (z2 == z) {
+                left = middle + 1;
             } else {
-                low = mid;
+                right = middle;
             }
         }
 
-        System.out.println(ans);
-    }
-
-
-    static int getPercent(int x, int y) {
-        return (int) ((long) y * 100 / x);
+        System.out.println(left);
     }
 }
