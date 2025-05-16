49cf1f07b931bf62ddc144e4437e18577fd434e1
김도선 <ehtjsv2@naver.com>
2025-05-16 14:48:01 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.05 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -2,64 +2,21 @@ import java.util.*;
 
 class Solution {
     
-    public int solution(int[] players, int m, int k) {
-        Server server = new Server(m, k);
-        for(int i = 0; i < players.length; i++){
-            // server update
-            server.returnServer(i);
-            // server extends
-            server.extendsServer(i,players[i]);
-        }
-        return server.answer;
-    }
-    
-    
-    public static class Server{
-        int serverCount = 1;
-        int m;
-        int k;
+    public int solution(int players[], int m, int k) {
+        int serverCount = 0;
+        int[] timeToRemoveCount = new int[50];
         int answer = 0;
-        PriorityQueue<ExtendServerTime> extendsTime = new PriorityQueue<>(
-            (o1, o2)->{
-                ExtendServerTime e1 = (ExtendServerTime) o1;
-                ExtendServerTime e2 = (ExtendServerTime) o2;
-                return e1.time-e2.time;
+        
+        for(int i = 0; i < players.length; i++){
+            serverCount -= timeToRemoveCount[i];
+            int remain = players[i]-(m*serverCount);
+            while(remain > 0 && remain>=m){
+                serverCount++;
+                remain -= m;
+                answer ++;
+                timeToRemoveCount[i+k]++;
             }
-        );
-        
-        public Server(int m, int k){
-            this.m = m;
-            this.k = k;
         }
-        
-        public void returnServer(int time){
-            while(!extendsTime.isEmpty() && (extendsTime.peek().time + k) <= time){
-                serverCount-=extendsTime.peek().count;
-                extendsTime.remove();
-            }
-            
-        }
-        
-        public void extendsServer(int time, int userCount){
-            if(userCount >= (m*serverCount)){
-                int needServerCount = (userCount / m) + 1;
-                int extendsCount = needServerCount - serverCount;
-                answer+=extendsCount;
-                serverCount=needServerCount;
-                extendsTime.add(new ExtendServerTime(time, extendsCount));
-                System.out.println(time+", "+userCount+", "+(serverCount-1)+", "+extendsCount);
-            }
-            
-        }
-    }
-    
-    public static class ExtendServerTime{
-        int time;
-        int count;
-        
-        public ExtendServerTime(int t, int c){
-            this.time = t;
-            this.count = c;
-        }
+        return answer;
     }
 }
